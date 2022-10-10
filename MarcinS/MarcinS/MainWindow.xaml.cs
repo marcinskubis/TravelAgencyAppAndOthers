9c7bba6d952a020ad7668cca34f16a351a4cb88e
marcinskubis <marcinskubis@interia.eu>
2022-10-10 19:19:25 +0200

    kalkulator1

--- MarcinS/MarcinS/MainWindow.xaml.cs
+++ MarcinS/MarcinS/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,79 @@ namespace MarcinS
         public MainWindow()
         {
             InitializeComponent();
+        }
+        #region klawisze
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text=input.Text+"1";
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "4";
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "7";
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "2";
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "5";
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "8";
+        }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "0";
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "3";
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "6";
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            input.Text = input.Text + "9";
+
+        }
+        #endregion
+
+        #region funckje
+
+
+
+        #endregion
+
+        private void input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void input_previewtextinput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
     }
 }
