0704b5b5524925e141104a6a0871fcb2bbca9731
marcinskubis <marcin.skubis@interia.eu>
2022-10-12 16:39:29 +0200

    lista_2-początek

---
+++ MarcinS/MarcinS/HomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarcinS
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void calculatorClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow zad1 = new MainWindow();
+            zad1.Show();
+        }
+
+        private void kikClick(object sender, RoutedEventArgs e)
+        {
+            Kik kik=new Kik();
+            kik.Show();
+        }
+
+        private void shipsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MarcinS/MarcinS/Kik.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarcinS
+{
+    /// <summary>
+    /// Interaction logic for Kik.xaml
+    /// </summary>
+    public partial class Kik : Window
+    {
+        public Kik()
+        {
+            InitializeComponent();
+        }
+
+        private void r1c1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void r1c2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r1c3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r2c1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r2c2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r2c3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r3c1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r3c2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void r3c3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
