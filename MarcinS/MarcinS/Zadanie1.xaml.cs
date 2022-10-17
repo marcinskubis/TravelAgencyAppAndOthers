ad59dcae1e059584b7472c10cbd2210d26861145
marcinskubis <marcinskubis@interia.eu>
2022-10-17 17:07:52 +0200

    Kalkulator: poprawione działanie i dodanie liczb z przecinkiem

--- MarcinS/MarcinS/Zadanie1.xaml.cs
+++ MarcinS/MarcinS/Zadanie1.xaml.cs
@@ -11,10 +11,11 @@ namespace MarcinS
         public MainWindow()
         {
             InitializeComponent();
+            input.Text = "0";
         }
         #region zmienne
         string lastOperation = "";
-        int result = 0;
+        double result = 0;
         bool operation_pressed = false;
 
         #endregion
@@ -25,35 +26,72 @@ namespace MarcinS
         
         private void add_Click(object sender, RoutedEventArgs e)
         {
-            lastOperation="+";
-            result = Convert.ToInt32(input.Text);
+            if (result != 0)
+            {
+                equ_Click(sender, e);
+                lastOperation = "+";
+                operation_pressed = true;
+                result = Convert.ToDouble(input.Text);
+                screen.Content = Convert.ToString(result) + lastOperation;
+            }
+            lastOperation = "+";
             operation_pressed = true;
+            result = Convert.ToDouble(input.Text);
+            screen.Content = Convert.ToString(result) + lastOperation;
         }
 
         private void sub_Click(object sender, RoutedEventArgs e)
         {
-            lastOperation="-";
-            result = Convert.ToInt32(input.Text);
+            if (result != 0)
+            {
+                equ_Click(sender, e);
+                lastOperation = "-";
+                operation_pressed = true;
+                result = Convert.ToDouble(input.Text);
+                screen.Content = Convert.ToString(result) + lastOperation;
+            }
+            lastOperation = "-";
             operation_pressed = true;
+            result = Convert.ToDouble(input.Text);
+            screen.Content = Convert.ToString(result) + lastOperation;
         }
 
         private void mul_Click(object sender, RoutedEventArgs e)
         {
-            lastOperation="*";
-            result = Convert.ToInt32(input.Text);
+            if (result != 0)
+            {
+                equ_Click(sender, e);
+                lastOperation = "*";
+                operation_pressed = true;
+                result = Convert.ToDouble(input.Text);
+                screen.Content = Convert.ToString(result) + lastOperation;
+            }
+            lastOperation = "*";
             operation_pressed = true;
+            result = Convert.ToDouble(input.Text);
+            screen.Content = Convert.ToString(result) + lastOperation;
         }
 
         private void div_Click(object sender, RoutedEventArgs e)
         {
-            lastOperation="/";
-            result = Convert.ToInt32(input.Text);
+            if (result != 0)
+            {
+                equ_Click(sender, e);
+                lastOperation = "/";
+                operation_pressed = true;
+                result = Convert.ToDouble(input.Text);
+                screen.Content = Convert.ToString(result) + lastOperation;
+            }
+            lastOperation = "/";
             operation_pressed = true;
+            result = Convert.ToDouble(input.Text);
+            screen.Content = Convert.ToString(result) + lastOperation;
         }
 
         private void clear_Click(object sender, RoutedEventArgs e)
         {
-            input.Text = "";
+            input.Text = "0";
+            result = 0;
         }
 
         private void equ_Click(object sender, RoutedEventArgs e)
@@ -61,36 +99,49 @@ namespace MarcinS
             switch (lastOperation)
             {
                 case"+":
-                    input.Text = Convert.ToString(result + Convert.ToInt32(input.Text));
+                    input.Text = Convert.ToString(result + Convert.ToDouble(input.Text));
                     break;
                 case "-":
-                    input.Text = Convert.ToString(result - Convert.ToInt32(input.Text));
+                    input.Text = Convert.ToString(result - Convert.ToDouble(input.Text));
                     break;
                 case "*":
-                    input.Text = Convert.ToString(result * Convert.ToInt32(input.Text));
+                    input.Text = Convert.ToString(result * Convert.ToDouble(input.Text));
                     break;
                 case "/":
-                    input.Text = Convert.ToString(result / Convert.ToInt32(input.Text));
+                    input.Text = Convert.ToString(result / Convert.ToDouble(input.Text));
                     break;
                 default:
                     break;
             }
+            result = Convert.ToDouble(input.Text);
+            screen.Content = result;
+            lastOperation = "";
             
         }
 
-        private void zerzer_Click(object sender, RoutedEventArgs e)
+        
+        private void coma_Click(object sender, RoutedEventArgs e)
         {
             if (operation_pressed)
             {
                 input.Clear();
             }
-            operation_pressed = false;
-            input.Text = input.Text + "00";
+            else if (String.IsNullOrEmpty(input.Text))
+            {
+                input.Text = input.Text + "0";
+            }
+            if (!input.Text.Contains(",")) {
+                input.Text = input.Text + ",";
+            }
+            else
+            {
+                MessageBox.Show("Niedozwolona operacja");
+            }
         }
 
         private void one_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed||input.Text=="0")
             {
                 input.Clear();
             }
@@ -101,7 +152,7 @@ namespace MarcinS
 
         private void four_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -112,7 +163,7 @@ namespace MarcinS
 
         private void seven_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -124,7 +175,7 @@ namespace MarcinS
         private void two_Click(object sender, RoutedEventArgs e)
         {
 
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -135,7 +186,7 @@ namespace MarcinS
 
         private void five_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -146,7 +197,7 @@ namespace MarcinS
 
         private void eight_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -157,7 +208,7 @@ namespace MarcinS
 
         private void zero_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -168,7 +219,7 @@ namespace MarcinS
 
         private void three_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -179,7 +230,7 @@ namespace MarcinS
 
         private void six_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -190,7 +241,7 @@ namespace MarcinS
 
         private void nine_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed)
+            if (operation_pressed || input.Text == "0")
             {
                 input.Clear();
             }
@@ -210,6 +261,5 @@ namespace MarcinS
             e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
 
-        
     }
 }
