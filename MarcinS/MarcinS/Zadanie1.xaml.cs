5fb0b9c55b0834543ad5e0d0d39d5585397976a1
marcinskubis <marcinskubis@interia.eu>
2023-01-11 16:27:01 +0100

    poprawka

--- MarcinS/MarcinS/Zadanie1.xaml.cs
+++ MarcinS/MarcinS/Zadanie1.xaml.cs
@@ -140,127 +140,20 @@ namespace MarcinS
             }
         }
 
-        private void one_Click(object sender, RoutedEventArgs e)
+        private void number_Click(object sender, RoutedEventArgs e)
         {
-            if (operation_pressed||input.Text=="0")
+            Button btn = (Button)sender;
+            if (operation_pressed || input.Text == $"0")
             {
                 input.Clear();
             }
             operation_pressed = false;
-            input.Text = input.Text + "1";
-            
-        }
-
-        private void four_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "4";
-            
-        }
-
-        private void seven_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "7";
-            
-        }
-
-        private void two_Click(object sender, RoutedEventArgs e)
-        {
-
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "2";
-            
-        }
-
-        private void five_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "5";
-            
-        }
-
-        private void eight_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "8";
-            
-        }
-
-        private void zero_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "0";
-            
-        }
-
-        private void three_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "3";
-            
-        }
-
-        private void six_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "6";
-            
-        }
-
-        private void nine_Click(object sender, RoutedEventArgs e)
-        {
-            if (operation_pressed || input.Text == "0")
-            {
-                input.Clear();
-            }
-            operation_pressed = false;
-            input.Text = input.Text + "9";
-            
+            input.Text = input.Text + $"{btn.Content}";
         }
         #endregion
-
-        private void input_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
         private void input_previewtextinput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
-
     }
 }
